6d4ec141656e26fd739bbf4d08c04d4424a1dc71
r.aramide <r.aramide@lagetronix.com>
2024-01-01 23:04:59 +0100

    added more endpoints

--- controllers/statistics.controller.js
+++ controllers/statistics.controller.js
@@ -2,7 +2,7 @@ const StatisticsModel = require("../models/statistics.model");
 
 async function getStatistics(req, res, next) {
   try {
-    const data = await StatisticsModel.find();
+    const data = await StatisticsModel.find().limit(30);
 
     res.status(200).json(data);
   } catch (error) {
@@ -11,4 +11,118 @@ async function getStatistics(req, res, next) {
   }
 }
 
-module.exports = { getStatistics };
+async function getIntensity(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("intensity published")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getLikelihood(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("likelihood published")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getRelevance(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("relevance published")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getYear(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("added relevance")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getCountry(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("country intensity")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getTopics(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("topic relevance")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getRegion(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("region intensity")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function getCity(req, res, next) {
+  try {
+    const data = await StatisticsModel.find()
+      .select("intensity published")
+      .limit(20);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+module.exports = {
+  getStatistics,
+  getIntensity,
+  getCity,
+  getCountry,
+  getLikelihood,
+  getRegion,
+  getRelevance,
+  getTopics,
+  getYear,
+};
--- routes/statistics.route.js
+++ routes/statistics.route.js
@@ -4,5 +4,13 @@ const statisticsController = require("../controllers/statistics.controller");
 const statisticsRouter = express.Router();
 
 statisticsRouter.get("/all", statisticsController.getStatistics);
+statisticsRouter.get("/intensity", statisticsController.getIntensity);
+statisticsRouter.get("/city", statisticsController.getCity);
+statisticsRouter.get("/country", statisticsController.getCountry);
+statisticsRouter.get("/likelihood", statisticsController.getLikelihood);
+statisticsRouter.get("/region", statisticsController.getRegion);
+statisticsRouter.get("/relevance", statisticsController.getRelevance);
+statisticsRouter.get("/topics", statisticsController.getTopics);
+statisticsRouter.get("/year", statisticsController.getYear);
 
 module.exports = statisticsRouter;
